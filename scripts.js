991d34b3f260afcec93716686bddeb7c6cb43912
brunnorossetti <brunno@songsay.com>
2024-06-19 11:26:30 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted successfully!');
+
+    // Add form submission handling logic here
+});
